08ebf0af7271cfd1a8119fe1fa1e0ffe31cf7ca0
PiraTechnics <devin1younge@gmail.com>
2023-04-27 14:51:15 -0700

    Bootstrap Table, Logic List Refactor

--- src/ui.js
+++ src/ui.js
@@ -1,42 +1,80 @@
 // Module to build UI components with applicable Bootstrap classes
 
 function listItem(Todo, listIndex) {
-    const entryContainer  = document.createElement('li');
-    entryContainer.setAttribute('class', 'todo-entry list-group-item');
+    const entryContainer = document.createElement('tr');
+    const entryCheckBox = createTableRowElement(createCheckBox(listIndex), 'td');
+    const entryTitle = createTableRowElement(document.createTextNode(Todo.title), 'td');
+    const entryDue = createTableRowElement(document.createTextNode(Todo.dueDate), 'td');
+    const entryImp = createTableRowElement(document.createTextNode(Todo.priority), 'td');
+    const entryUrg = createTableRowElement(document.createTextNode(Todo.urgent), 'td');
+    entryContainer.append(entryCheckBox, entryTitle, entryDue, entryImp, entryUrg);
 
-    const entryId = 'checkbox-' + listIndex;
-
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute('class', 'form-check-input ms-1 me-3');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.setAttribute('vlaue', '');
-    checkbox.setAttribute('id', entryId);
-
-    const checkLabel = document.createElement('label');
-    checkLabel.setAttribute('class', 'form-check-label');
-    checkLabel.setAttribute('for', entryId);
-    checkLabel.innerText = Todo.title;
-
-    entryContainer.append(checkbox, checkLabel);
     return entryContainer;
+}
+
+function createTableRowElement(entry, type, scope) {
+    const element = document.createElement(type);
+    element.setAttribute('scope', scope);
+    element.appendChild(entry);
+
+    return element;
+}
+
+function createCheckBox(listIndex) {
+    const checkContainer = document.createElement('div');
+    checkContainer.setAttribute('class', 'form-check');
+    
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('class', 'form-check-input ms-1 me-3');
+    checkBox.setAttribute('type', 'checkBox');
+    checkBox.setAttribute('vlaue', '');
+
+    if(!listIndex) {
+        const id = 'checkbox-' + listIndex;
+        checkBox.setAttribute('id', id);
+    }
+    else {
+        checkBox.setAttribute('id', 'placeholder');
+    }
+
+    checkContainer.append(checkBox);
+    return checkContainer;
 }
 
 export function list(todoList, name) {
     const listContainer = document.createElement('div');
     listContainer.setAttribute('class', 'listContainer');
-    const listHeader = document.createElement('p');
-    listHeader.setAttribute('class', 'h3 text-center listHeader');
-    listHeader.appendChild(document.createTextNode(name));
-    listContainer.appendChild(listHeader);
 
-    const domList = document.createElement('ul');
-    domList.setAttribute('class', 'list-group list-group-flush m-2');
+    //Create Table and link structure in DOM
+    const table = document.createElement('table');
+    table.setAttribute('class', 'table');
+    
+    //Create Table Header and link Elements in DOM
+    const th = document.createElement('thead');
+    const thRow = document.createElement('tr');
+    th.append(thRow);
+    table.append(th);
 
-    for (let i = 0; i < todoList.size; i++) {
-        domList.appendChild(listItem(todoList.list[i], i));
-        console.log("Entry added: " + JSON.stringify(todoList.list[i])); //Console Sanity check
+    //Create and add Header Row to Table
+    const checkBox = createCheckBox('placeholder');
+    checkBox.classList.add('invisible');
+    const checkKey = createTableRowElement(checkBox, 'th', 'col');
+    const itemKey = createTableRowElement(document.createTextNode('Item'), 'th', 'col');
+    const dueDateKey = createTableRowElement(document.createTextNode('Due'), 'th', 'col');
+    const impKey = createTableRowElement(document.createTextNode('Important'), 'th', 'col');
+    const urgKey = createTableRowElement(document.createTextNode('Urgent'), 'th', 'col');
+    thRow.append(checkKey, itemKey, dueDateKey, impKey, urgKey);
+
+    //Create Table Body, add list elements to it, and link in DOM
+    const tBody = document.createElement('tbody');
+    tBody.setAttribute('class', 'table-group-divider');
+    table.append(tBody);
+
+    for(let i = 0; i < todoList.length; i++) {
+        tBody.appendChild(listItem(todoList[i], i));
+        console.log("Entry added: " + JSON.stringify(todoList[i]));
     }
 
-    listContainer.appendChild(domList);
+    listContainer.appendChild(table);
     return listContainer;
 }
